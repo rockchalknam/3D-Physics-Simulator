d455d77b9efe068d8c3ba8f02ef76bf2c6a9f476
rockchalknam <nam.nguyen@ridgefieldsd.org>
2026-03-24 19:13:04 -0700

    Update romlist.js

--- romlist.js
+++ romlist.js
@@ -1,7 +1,3 @@
 var ROMLIST = [
-    /*
-    {url:"roms/rom1.z64",title:"Game 1"},
-    {url:"roms/rom2.v64",title:"Game 2"},
-    {url:"roms/rom3.v64",title:"Game 3"},
-    */
+    {url: "practice_engine.dat", title: "Kinematics Practice Module 1"}
 ];
